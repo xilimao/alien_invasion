ae53eabecec33d597a04f4c50d0c2868f31d3d13
xilimao <caxilimao@gmail.com>
2019-01-02 22:55:57 +0800

    tow

---
+++ bullet.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """一个对飞船发射的子弹进行管理的类"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """在飞船所处的位置创建一个子弹对象"""
+        super(Bullet, self).__init__()
+        self.screen = screen
